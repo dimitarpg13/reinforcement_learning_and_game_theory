e5148a796266c25d8ce9175c5995466ae24117e7
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2023-07-08 23:44:08 -0400

    demo code

---
+++ src/experiments/openai/gym/gym_demo/run_gym_demo.py
@@ -0,0 +1,17 @@
+import gym
+
+env = gym.make('MountainCar-v0', render_mode="human")
+observation = env.reset()
+t = 0
+while True:
+     t += 1
+     env.render()
+     observation = env.reset()
+     print(observation)
+     action = env.action_space.sample()
+     observation, reward, done, info, _ = env.step(action)
+     if done:
+         print("Episode finished after {} timesteps".format(t+1))
+         break
+
+env.close()
