ef0584784832510120550d953f0ac1e197d0a08a
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2023-07-12 02:00:06 -0400

    doc update

--- src/experiments/openai/gym/gym_demo/lunar_lander_random.py
+++ src/experiments/openai/gym/gym_demo/lunar_lander_random.py
@@ -1,15 +1,17 @@
 import gym
 import random
 
+MAX_EPISODE_TIMESTEPS = 10000
 env = gym.make("LunarLander-v2",render_mode="human")
 
 def Random_games():
-    # each of this episode is its own game
+    # each of these episodes is a new game
     for episode in range(10):
         env.reset()
 
-        # this is each frame, up to 500
-        while True:
+        # perform up to MAX_EPISODE_TIMESTEPS, generating that number of frames
+        t = 0
+        while t < MAX_EPISODE_TIMESTEPS:
             # This will display the environment
             # Only display if you really want to see it
             # Takes much longer to display it
@@ -23,6 +25,8 @@ def Random_games():
             # and returns the observation of the environment,
             # the reward, if the env is over, and other info.
             next_state, reward, done, info, _ = env.step(action)
+
+            t = t + 1
 
             # lets print everything in one line:
             print(next_state, reward, done, info, action)
--- src/experiments/openai/gym/gym_demo/mountain_car_random.py
+++ src/experiments/openai/gym/gym_demo/mountain_car_random.py
@@ -1,19 +1,39 @@
 import gym
 import random
+import time
 
+MAX_EPISODE_TIMESTEPS = 50
 env = gym.make('MountainCar-v0', render_mode="human")
-observation = env.reset()
-t = 0
-while True:
-     t += 1
-     env.render()
-     observation = env.reset()
-     print(observation)
-     random_action = env.action_space.sample()
-     observation, reward, done, info, _ = env.step(random_action)
-     if done or t == 500:
-         print("Episode finished after {} timesteps".format(t+1))
-         break
 
-env.close()
+
+def Random_games():
+    # each of these episodes is another game
+    for episode in range(10):
+        env.reset()
+
+        # perform up to MAX_EPISODE_TIMESTEPS, generating that number of frames
+        t = 0
+        while t < MAX_EPISODE_TIMESTEPS:
+
+            # displays the ennvironment in the current frame
+            env.render()
+
+            # create a sample random action in any environment
+            random_action = env.action_space.sample()
+
+            # this executes the environment with an action,
+            # and returns the observation of the environment,
+            # the reward, if the env is over, and other info.
+            next_state, reward, done, info, _ = env.step(random_action)
+
+            t = t + 1
+
+            print(next_state, reward, done, info, random_action)
+            if done:
+                break
+
+        time.sleep(1)
+    env.close()
+
+Random_games()
 
